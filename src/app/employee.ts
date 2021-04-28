651549cbef16be4a2d266bca8477c0afc77def5d
uuldanaa <44373793+uuldanaa@users.noreply.github.com>
2021-04-29 04:32:56 +0600

    commit

---
+++ src/app/employee.ts
@@ -0,0 +1,13 @@
+export class List {
+  id: number;
+  name: string;
+  status: string;
+
+  constructor(id: number,
+  name: string,
+  status: string) {
+    this.id = id;
+    this.name = name;
+    this.status = status;
+  }
+}
